91a1aa30dd6783885fd260136f37c50699cf2e81
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-09 19:55:20 -0300

    model ativo + enums implementados

--- src/model/ativo.ts
+++ src/model/ativo.ts
@@ -1,5 +1,17 @@
 import { AbstractEntity } from "./abstract-entity"
+import { Categoria } from "./categoria"
+import { Condicao } from "./condicao"
+import { Status } from "./status"
 
 export class Ativo extends AbstractEntity {
+
+
+    categoria! : Categoria
+    idPatrimonio! : string
+    condicao! : Condicao
+    status! : Status
+    descricao! : string
+    dataEntrada! : Date
+
 
 }
---
+++ src/model/status.ts
@@ -0,0 +1,6 @@
+export enum Status {
+    DISPONIVEL = "DISPONIVEL",
+    USANDO = "USANDO",
+    MANUTENCAO = "MANUTENCAO",
+    INUTILIZAVEL = "INUTILIZAVEL"
+}
