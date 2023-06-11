d09d45e96e40c3dcf958f9ca508764294c4e3e5e
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-06-11 19:19:47 -0300

    Iniciando a aba de cadastrar Categoria

---
+++ src/views/CategoriaCadastrarView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-content d-flex flex-column align-items-start gap-5">
+    <div class="page-header d-flex justify-content-between align-items-center">
+      <LinkDinamicoComponent routeList="/categorias" routeRegister="/categorias/cadastrar" />
+
+    </div>
+</div>
+    
+  </template> 
+
+
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+
+  
+  import LinkDinamicoComponent from '@/components/LinkDinamicoComponent.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'CategoriaCadastrar',
+  components: {
+    LinkDinamicoComponent,
+  },
+  });
+  </script>
+  
