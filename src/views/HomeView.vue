2491a3b15776eee1968f18b4f8c34e4a0cb4254d
vainicius <comercial.viniciusgabriel@gmail.com>
2023-06-11 21:57:44 -0300

    Iniciando HTML

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,52 @@
 <template>
-  <h1>Home</h1>
-</template>
+  <div class="main-content">
+   <body>
+    <div class="caroussel">
+      <h1>imagina um carrousel aqui uau</h1>
+      <hr id="linha-baixo-carrousel">
+    </div>
+    <div class="table-01">
+      <table>
+        <thead>
+          <tr>
+            <th>Ativos em atraso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ativo 01</td>
+          </tr>
+          <tr>
+            <td>ativo 02</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="table-02">
+      <table>
+        <thead>
+          <tr>
+            <th>Últimas ações</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>Ação 01</td>
+            </tr>
+            <tr>
+              <td>Ação 02</td>
+            </tr>
+          </tbody>
+      </table>  
+    </div>
+   </body>
+    
+    </div>
+    
+  </template>
+
+
+
+
 
 
