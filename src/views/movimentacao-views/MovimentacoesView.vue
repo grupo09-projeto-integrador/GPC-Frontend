512034c9c048dec3a7be94f9fa019296a9a8e714
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-13 20:34:18 -0300

    mudanca de porta

--- src/views/movimentacao-views/MovimentacoesView.vue
+++ src/views/movimentacao-views/MovimentacoesView.vue
@@ -73,14 +73,14 @@
           />
         </div>
         <div class="form-check align-self-end mb-2">
-        <input
-          id="isDevolvido"
-          class="form-check-input"
-          type="checkbox"
-          v-model="movimentacao.isDevolvido"
-        />
-        <label class="m-0" for="isDevolvido">Mostrar devolvidos</label>
-      </div>
+          <input
+            id="isDevolvido"
+            class="form-check-input"
+            type="checkbox"
+            v-model="movimentacao.isDevolvido"
+          />
+          <label class="m-0" for="isDevolvido">Mostrar devolvidos</label>
+        </div>
       </div>
 
       <!-- </div> -->
@@ -94,6 +94,48 @@
         </button>
       </div>
     </form>
+    <div class="table-display mt-4 w-100">
+      <table class="table table-sm table-bordered w-100">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Id Categoria</th>
+            <th scope="col">Id Património</th>
+            <th scope="col">Condição</th>
+            <th scope="col">Status</th>
+            <th scope="col">Data Entrada</th>
+            <th scope="col">Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="ativo in ativosFilter" :key="ativo.id">
+            <td>{{ ativo.id }}</td>
+            <td>{{ ativo.categoria.id }}</td>
+            <td>{{ ativo.idPatrimonio }}</td>
+            <td>{{ ativo.condicao }}</td>
+            <td>{{ ativo.status }}</td>
+            <td>{{ formatDate(ativo.dataEntrada) }}</td>
+
+            <td>
+              <div class="d-flex justify-content-center actions">
+                <button
+                  class="btn btn-sm btn-primary me-2"
+                  @click="editItem(ativo)"
+                >
+                  <i class="bi bi-pencil-square"></i> Editar
+                </button>
+                <button
+                  class="btn btn-sm btn-danger"
+                  @click="deleteItem(ativo)"
+                >
+                  <i class="bi bi-trash"></i> Excluir
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
