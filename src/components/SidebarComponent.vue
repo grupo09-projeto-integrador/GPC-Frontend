c2eeaa880d6b0ed84bf9510d080bdd6b7fddb23e
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-18 18:34:17 -0300

    general optimizations.

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,59 +1,36 @@
 <template>
-  <div
-    class="navbar bg-primary d-flex flex-column p-3 text-white h-100"
-    style="width: 250px"
-  >
-    <a
-      href="/"
-      class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-    >
+  <div class="navbar bg-primary d-flex flex-column justify-content-start p-4 text-white h-100">
+    <a href="/" class="d-flex align-items-center">
       <img alt="Logo Empresa" src="../assets/logo.png" />
     </a>
-    <hr />
-    <hr />
-    <ul class="nav d-flex align-items-start nav-pills flex-column mb-auto">
+    <ul class="nav nav-pills d-flex flex-column justify-content-center align-items-start pt-5">
       <li class="nav-item d-flex align-items-center justify-content-center">
         <i class="bi bi-house-fill"></i>
-        <router-link class="nav-link text-white" to="/inicio"
-          >Inicio</router-link
-        >
+        <router-link class="nav-link text-white" to="/inicio">Inicio</router-link>
       </li>
       <li class="nav-item d-flex align-items-center justify-content-center">
-        <i class="bi bi-people-fill"></i
-        ><router-link class="nav-link text-white" to="/usuarios"
-          >Pessoas</router-link
-        >
+        <i class="bi bi-people-fill"></i><router-link class="nav-link text-white" to="/usuarios">Pessoas</router-link>
       </li>
       <li class="nav-item d-flex align-items-center justify-content-center">
-        <i class="bi bi-bookmark-fill"></i
-        ><router-link class="nav-link text-white" to="/categorias"
-          >Categorias</router-link
-        >
+        <i class="bi bi-bookmark-fill"></i><router-link class="nav-link text-white"
+          to="/categorias">Categorias</router-link>
       </li>
       <li class="nav-item d-flex align-items-center justify-content-center">
-        <i class="bi bi-archive-fill"></i
-        ><router-link class="nav-link text-white" to="/ativos"
-          >Ativos</router-link
-        >
+        <i class="bi bi-archive-fill"></i><router-link class="nav-link text-white" to="/ativos">Ativos</router-link>
       </li>
       <li class="nav-item d-flex align-items-center justify-content-center">
-        <i class="bi bi-calendar-check-fill"></i
-        ><router-link class="nav-link text-white" to="/movimentacoes"
-          >Movimentações</router-link
-        >
+        <i class="bi bi-calendar-check-fill"></i><router-link class="nav-link text-white"
+          to="/movimentacoes">Movimentações</router-link>
       </li>
       <li class="nav-item d-flex align-items-center justify-content-center">
-        <i class="bi bi-clipboard2-data-fill"></i
-        ><router-link class="nav-link text-white" to="/relatorios"
-          >Relatórios</router-link
-        >
+        <i class="bi bi-clipboard2-data-fill"></i><router-link class="nav-link text-white"
+          to="/relatorios">Relatórios</router-link>
       </li>
     </ul>
-    <hr />
-    <li class="nav-item d-flex align-items-center justify-content-center">
+    <div class="mt-auto d-flex align-items-center justify-content-center gap-3">
       <i class="logout bi bi-box-arrow-right"></i>
       <a href="/logout" class="nav-link text-white">Logout</a>
-    </li>
+    </div>
   </div>
 </template>
 
@@ -67,6 +44,7 @@ export default defineComponent({
 <style scoped>
 .navbar {
   height: 100%;
+  width: 250px;
 }
 
 a {
