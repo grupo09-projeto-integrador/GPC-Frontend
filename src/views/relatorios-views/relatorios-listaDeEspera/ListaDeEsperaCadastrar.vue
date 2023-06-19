4bed9dc84814bc60dfb99a4db5170cfc743377f0
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-19 11:50:14 -0300

    lista de espera | structure done.

--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaCadastrar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaCadastrar.vue
@@ -2,7 +2,7 @@
     <div class="main-content">
         <LinkDinamicoComponent routeList="/relatorios/listadeespera" routeRegister="/relatorios/listadeespera/cadastrar"
             default-active="register" />
-        <form class="form-app d-flex flex-column align-items-start mt-4 gap-4">
+        <form class="form-app d-flex flex-column align-items-start mt-4 gap-4" @submit.prevent="submitForm">
             <div class="d-flex align-items-center align-self-start gap-4">
                 <div class="d-flex flex-column align-self-start gap-2">
                     <label for="beneficiario">CPF do Beneficiario</label>
@@ -22,21 +22,8 @@
             <div class="d-flex align-items-center align-self-start gap-5">
                 <div class="d-flex flex-column align-self-start gap-2">
                     <label for="categoria">Categoria do Ativo</label>
-                    <input class="form-control" list="datalistOptions" id="categoria" style="width: 300px" />
+                    <select class="form-select"  id="categoria" style="width: 300px"></select>
                 </div>
-                <datalist id="datalistOptions">
-                    <option v-for="option in datalistOptionsCategoria" :value="option"></option>
-                </datalist>
-                <div class="d-flex flex-column align-self-end gap-2">
-                    <label for="urgent">Nível de Urgência</label>
-                    <select class="form-select" id="urgent" style="width: 300px">
-                    </select>
-                </div>
-            </div>
-            <div class="descricao d-flex flex-column gap-2">
-                <label for="descricao">Adicione uma descrição (opcional)</label>
-                <textarea class="form-control" placeholder="Faça uma observação aqui" id="descricao"
-                    style="height: 120px;width: 800px;"></textarea>
             </div>
             <div class="mt-3 d-flex align-items-center gap-3">
                 <button type="submit" class="btn btn-primary">Adicionar Beneficiario</button>
@@ -48,6 +35,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
+import { Categoria } from '@/model/categoria';
+import { Pessoa } from '@/model/pessoa';
+import { CategoriaClient } from '@/client/categoria.client';
+import { Beneficiario } from '@/model/beneficiario';
+import { BeneficiarioClient } from '@/client/beneficiario.client';
 export default defineComponent({
     name: "ListaDeEsperaCadastrar",
     components: {
@@ -57,7 +49,39 @@ export default defineComponent({
         return {
             datalistOptions: [] as string[],
             datalistOptionsCategoria: [] as string[],
+            categoria: [] as Categoria[],
+            listaDeEspera: [],
+            beenficiario: [] as Beneficiario[],
+            nivelUrgencia: [
+                { id: 1, nome: 'Alta' },
+                { id: 2, nome: 'Média' },
+                { id: 3, nome: 'Baixa' },
+            ],
         };
+    },
+    computed:{
+        categoriaClient(){
+            return new CategoriaClient();
+        },
+        beneficiarioClient(){
+            return new BeneficiarioClient();
+        }
+    },
+//     async mounted() {
+//     try {
+//       const beneficiarioClient = new BeneficiarioClient();
+//       const data = await beneficiarioClient.findAll();
+//       this.datalistOptions = data.map((beneficiario: { perfil: { cpf: string; }; }) => beneficiario.perfil.cpf);
+//       console.log(this.datalistOptions);
+//     } catch (error) {
+//       console.error("Failed to fetch cpf data:", this.datalistOptions);
+//     }
+//   },
+    methods: {
+        async submitForm() {
+        },
+        async fetchBeneficiario() {
+        },
     },
 });
 </script>
--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
@@ -4,52 +4,42 @@
             <LinkDinamicoComponent routeList="/relatorios/listadeespera"
                 routeRegister="/relatorios/listadeespera/cadastrar" />
             <div class="search-container mt-3">
-                <input type="text" class="search-input" placeholder="Buscar Por Nome do Beneficiario..." v-model="searchQuery" />
+                <input type="text" class="search-input" placeholder="Buscar Por Nome do Beneficiario..."
+                    v-model="searchQuery" />
                 <i class="bi bi-search search-icon"></i>
             </div>
         </div>
         <form class="form-app d-flex flex-column align-items-start gap-3 mt-4">
-      <div class="d-flex align-items-center align-self-start gap-4">
+            <div class="d-flex align-items-center align-self-start gap-4">
 
-        <div class=" d-flex flex-column align-self-start">
-          <label for="urgencia_id">Nível de Urgência</label>
-          <select class="form-select"  style="width: 300px">
-          </select>
-        </div>
-
-        <div class=" d-flex flex-column align-self-start">
-            <label for="dt_entrada">Categoria</label>
-            <select class="form-select"  style="width: 300px">
-          </select>
-          </div>
-
-        <div class=" d-flex flex-column align-self-start ">
-            <label for="dt_entrada">Data do Registro</label>
-            <input type="date" class="form-control" id="dt_entrada" style="width: 300px" />
-        </div>
-      </div>
-    </form>
-            <table class="table table-sm table-bordered w-100 mt-4">
-                <thead>
-                    <tr>
-                        <th scope="col">Data do Registro</th>
-                        <th scope="col">Nome do Beneficiario</th>
-                        <th scope="col">Contato</th>
-                        <th scope="col">Categoria</th>
-                        <th scope="col">Nível de Urgência</th>
-                        <th scope="col">Descrição</th>
-                        <th scope="col">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
+                <div class=" d-flex flex-column align-self-start">
+                    <label for="dt_entrada">Categoria</label>
+                    <select class="form-select" style="width: 300px">
+                    </select>
+                </div>
+            </div>
+        </form>
+        <table class="table table-sm table-bordered w-100 mt-4">
+            <thead>
+                <tr>
+                    <th scope="col">Nome do Beneficiario</th>
+                    <th scope="col">CPF do Beneficiario</th>
+                    <th scope="col">Contato</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
+import { Categoria } from '@/model/categoria';
+import { Pessoa } from '@/model/pessoa';
 export default defineComponent({
     name: "ListaDeEsperaListar",
     components: {
@@ -58,6 +48,14 @@ export default defineComponent({
     data() {
         return {
             searchQuery: "",
+            categoria: [] as Categoria[],
+            listaDeEspera: [],
+            pessoa: [] as Pessoa[],
+            nivelUrgencia: [
+                { id: 1, nome: 'Alta' },
+                { id: 2, nome: 'Média' },
+                { id: 3, nome: 'Baixa' },
+            ],
         };
     },
 });
@@ -113,12 +111,12 @@ th {
     font-weight: 200;
     font-size: 18px;
     color: #ffffff !important;
-    background-color: #0067c8 !important; /* Replace with the same Bootstrap color as the <thead> background */
+    background-color: #0067c8 !important;
+    /* Replace with the same Bootstrap color as the <thead> background */
 }
 
 td {
     border: 1px solid #dddddd;
     text-align: center;
     padding: 8px;
-}
-</style>
+}</style>
