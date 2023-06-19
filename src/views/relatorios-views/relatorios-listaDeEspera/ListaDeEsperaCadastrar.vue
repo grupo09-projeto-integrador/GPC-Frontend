a6f12dd54bb615a415dfbae1be333495fc8e8a14
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-18 23:22:20 -0300

    lista de espera | inicial structure done.

--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaCadastrar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaCadastrar.vue
@@ -1,1 +1,51 @@
-<template></template>
+<template>
+    <div class="main-content">
+        <LinkDinamicoComponent routeList="/relatorios/listadeespera" routeRegister="/relatorios/listadeespera/cadastrar"
+            default-active="register" />
+        <form class="form-app d-flex flex-column align-items-start mt-4 gap-4">
+            <div class="d-flex align-items-center align-self-start gap-5">
+                <div class="d-flex flex-column align-self-start gap-2">
+                    <label for="beneficiario">Beneficiario</label>
+                    <input class="form-control" list="datalistOptions" id="beneficiario" style="width: 300px" />
+                </div>
+                <datalist id="datalistOptions">
+                    <option v-for="option in datalistOptions" :value="option"></option>
+                </datalist>
+                <router-link to="" class="btn btn-primary align-self-end">Cadastrar Beneficiario</router-link>
+            </div>
+            <div class="d-flex align-items-center align-self-start gap-5">
+                <div class="d-flex flex-column align-self-start gap-2">
+                    <label for="categoria">Categoria</label>
+                    <input class="form-control" list="datalistOptions" id="categoria" style="width: 300px" />
+                </div>
+                <datalist id="datalistOptions">
+                    <option v-for="option in datalistOptionsCategoria" :value="option"></option>
+                </datalist>
+                <div class="d-flex flex-column align-self-end gap-2">
+                    <label for="urgent">Nível de Urgência</label>
+                    <select class="form-select" id="urgent" style="width: 300px" >
+                    </select>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
+export default defineComponent({
+    name: "ListaDeEsperaCadastrar",
+    components: {
+        LinkDinamicoComponent,
+    },
+    data() {
+        return {
+            datalistOptions: [] as string[],
+            datalistOptionsCategoria: [] as string[],
+        };
+    },
+});
+</script>
+
+<style></style>
--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
@@ -1,1 +1,101 @@
-<template></template>
+<template>
+    <div class="main-content d-flex flex-column align-items-start">
+        <div class="page-header d-flex justify-content-between align-items-center">
+            <LinkDinamicoComponent routeList="/relatorios/listadeespera"
+                routeRegister="/relatorios/listadeespera/cadastrar" />
+            <div class="search-container mt-3">
+                <input type="text" class="search-input" placeholder="Buscar ..." v-model="searchQuery" />
+                <i class="bi bi-search search-icon"></i>
+            </div>
+        </div>
+            <table class="table table-sm table-bordered w-100 mt-4">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Data de Registro</th>
+                        <th scope="col">Nome do Beneficiario</th>
+                        <th scope="col">Contato do Beneficiario</th>
+                        <th scope="col">Categoria</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
+export default defineComponent({
+    name: "ListaDeEsperaListar",
+    components: {
+        LinkDinamicoComponent,
+    },
+    data() {
+        return {
+            searchQuery: "",
+        };
+    },
+});
+</script>
+
+<style>
+.page-header {
+    width: 100%;
+}
+
+.search-container {
+    position: relative;
+    margin-right: 30px;
+}
+
+.search-input {
+    position: relative;
+    width: 350px;
+    height: 36px;
+    padding: 0.5rem 1rem 0.5rem 2rem;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 50px;
+    background: #f5f5f5;
+}
+
+.search-input:focus {
+    outline: none;
+    border-color: #aaa;
+}
+
+.search-input::placeholder {
+    font-family: "Raleway", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    color: #3c3c43;
+}
+
+.search-icon {
+    position: absolute;
+    top: 50%;
+    right: 1rem;
+    transform: translateY(-50%);
+    color: #888;
+}
+
+
+th {
+    text-align: center;
+    height: 30px;
+    font-family: "Inter";
+    font-weight: 200;
+    font-size: 18px;
+    color: #ffffff !important;
+    background-color: #0067c8 !important; /* Replace with the same Bootstrap color as the <thead> background */
+}
+
+td {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+}
+</style>
