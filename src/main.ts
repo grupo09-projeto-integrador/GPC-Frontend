d922a64345e921eda87e6a37a39d5068da8a2130
vainicius <comercial.viniciusgabriel@gmail.com>
2023-06-25 23:15:59 -0300

    Configurando Bootstrap corretamente

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 
 const app = createApp(App)
 app.use(router).mount('#app')
--- src/views/home-view/HomeView.vue
+++ src/views/home-view/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
-  
-  <div class="main-content">
     
+  <div class="main-content">
+    <div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Calma, bença!</h4>
+    <p>O Carousel já vai sair!</p>
+    <hr>
+    <p class="mb-0">Enquanto ele não sai, use a imaginação aqui!</p>
+  </div>
     
     </div>
   
@@ -9,7 +14,7 @@
   </template>
 
   <style>
- @import url('../../../node_modules/bootstrap-icons/font/bootstrap-icons.css');
+ @import url('../../../node_modules/bootstrap-icons/font/bootstrap-icons.css')
 
 
 </style>
