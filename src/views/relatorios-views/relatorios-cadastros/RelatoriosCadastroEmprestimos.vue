797d84e5b805cdc2cb7d06c7d3868d492a7a73f4
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-30 10:09:01 -0300

    edits.

--- src/views/relatorios-views/relatorios-cadastros/RelatoriosCadastroEmprestimos.vue
+++ src/views/relatorios-views/relatorios-cadastros/RelatoriosCadastroEmprestimos.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-content d-flex flex-column">
-        <div class="printable ml-2">
+        <div class="printable ml-2 mr-2">
             <div class="header d-flex align-items-center justify-content-between">
                 <div class="titles">
                     <p class="title m-0">Relatórios de Cadastro | Emprestimos</p>
@@ -8,7 +8,25 @@
                     <p class="fromTo">De {{ from }} Até {{ to }}</p>
                 </div>
             </div>
-            <div class="data d-flex flex-column gap-2">
+            <div class="data">
+                <div class="summary-section">
+                    <div class="summary-item">
+                        <span>Total de Empréstimos:</span>
+                        <span>{{ moves.length }}</span>
+                    </div>
+                    <div class="summary-item">
+                        <span>Média de Duração do Empréstimo:</span>
+                        <span>{{ calculateAverageDuration }}</span>
+                    </div>
+                    <div class="summary-item">
+                        <span>Data de Empréstimo mais Recente:</span>
+                        <span>{{ getMostRecentLoanDate }}</span>
+                    </div>
+                    <div class="summary-item">
+                        <span>Data de Devolução mais Próxima:</span>
+                        <span>{{ getClosestReturnDate }}</span>
+                    </div>
+                </div>
             </div>
             <table class="table table-sm table-bordered w-100 mt-4">
                 <thead>
@@ -26,7 +44,7 @@
                 <tbody>
                     <tr v-for="move in moves" :key="move.id">
                         <td> {{ move.ativo.idPatrimonio }} </td>
-                        <td>{{ move.ativo.categoria.nomeCategoria}}</td>
+                        <td>{{ move.ativo.categoria.nomeCategoria }}</td>
                         <td>{{ move.beneficiario.perfil.nome }}</td>
                         <td>{{ move.beneficiario.perfil.cpf }}</td>
                         <td>{{ move.beneficiario.responsavel.nome }}</td>
@@ -64,6 +82,52 @@ export default defineComponent({
     },
     computed: {
 
+        calculateAverageDuration() {
+            if (this.moves.length === 0) {
+                return 0;
+            }
+            let totalDuration = 0;
+            this.moves.forEach(move => {
+                const startDate = new Date(move.dataEmprestimo);
+                const endDate = new Date(move.dataDevolucao);
+                const durationInDays = Math.ceil((Number(endDate) - Number(startDate)) / (1000 * 60 * 60 * 24));
+                totalDuration += durationInDays;
+            });
+            return (totalDuration / this.moves.length).toFixed(2);
+        },
+
+        getMostRecentLoanDate() {
+            if (this.moves.length === 0) {
+                return "";
+            }
+            const dates = this.moves.map(move => new Date(move.dataEmprestimo).getTime());
+            const maxDate = new Date(Math.max(...dates));
+            return this.formatDate(maxDate);
+        },
+
+        getClosestReturnDate() {
+            if (this.moves.length === 0) {
+                return "";
+            }
+            const dates = this.moves.map(move => new Date(move.dataEmprestimo).getTime());
+            const minDate = new Date(Math.min(...dates));
+            return this.formatDate(minDate);
+        },
+
+        getCategoryCounts() {
+            const categories = this.moves.map(move => move.ativo.categoria.nomeCategoria);
+            const categoryCounts = categories.reduce((acc, curr) => {
+                if (acc[curr]) {
+                    acc[curr]++;
+                } else {
+                    acc[curr] = 1;
+                }
+                return acc;
+            }, {} as { [key: string]: number });
+            return Object.keys(categoryCounts).map(key => ({ name: key, count: categoryCounts[key] }));
+        }
+
+
     },
     created() {
         // Access query parameters from the URL
@@ -78,25 +142,18 @@ export default defineComponent({
         downloadPDF() {
             const pdf = new jsPDF();
 
-            // Get the HTML content you want to convert
             const element = document.querySelector('.printable') as HTMLElement;
 
-            // Set the desired width and height for the PDF page
             const pdfWidth = pdf.internal.pageSize.getWidth();
             const pdfHeight = pdf.internal.pageSize.getHeight();
 
-            // Calculate the scaling factors based on the element's dimensions
             const widthRatio = pdfWidth / element.offsetWidth;
             const heightRatio = pdfHeight / element.offsetHeight;
             const scale = Math.min(widthRatio, heightRatio);
 
-
-            // Convert HTML to PDF using jsPDF with scaling options
             pdf.html(element, {
                 callback: function (pdf) {
-                    // Save the PDF file
-                    const pageNumber = 1; // Set the desired page number
-                    pdf.save(`report_page_${pageNumber}.pdf`);
+                    pdf.save(`ReportEmprestimo.pdf`);
                 },
                 x: 0,
                 y: 0,
@@ -114,7 +171,7 @@ export default defineComponent({
         async fetchMoves() {
             try {
                 const moveClient = new MovimentacoesClient();
-                const response = await moveClient.findByDatePdf(this.from,this.to)
+                const response = await moveClient.findByDatePdf(this.from, this.to)
                 this.moves = response;
             } catch (error) {
                 console.error(error);
@@ -151,5 +208,21 @@ th {
     font-size: 15px;
     color: #ffffff !important;
     background-color: #0067c8 !important;
+}
+.summary-section {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 1rem;
+}
+
+.summary-item {
+  flex-basis: 50%;
+  display: flex;
+  gap: 20px;
+  margin-bottom: 0.5rem;
+}
+
+.summary-item span:first-child {
+  font-weight: bold;
 }
 </style>
