4221a230820e1acda411af05423a8d115be3384d
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-07-03 10:05:25 -0300

    Merge branch 'vinicius' of github.com:grupo09-projeto-integrador/GPC-Frontend into jean

--- src/views/home-view/HomeView.vue
+++ src/views/home-view/HomeView.vue
@@ -1,35 +1,38 @@
 <template>
     
   <div class="main-content">
-  
-    <div class="alert alert-success" role="alert">
-    <h4 class="alert-heading">Calma, bença!</h4>
-    <p>O Carousel já vai sair!</p>
-    <hr>
-    <p class="mb-0">Enquanto ele não sai, use a imaginação aqui!</p>
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-            <button type="button" class="btn btn-success d-block w-100">Success</button>
-          </div>
-        <div class="carousel-item">
-          <button type="button" class="btn btn-danger d-block w-100">nao Success</button>
-
-        </div>
-        <div class="carousel-item">
-            <button type="button" class="btn btn-warning d-block w-100">talvez Success</button>
-          </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-            </div>    
-          </div>
+      <div class="container text-center">
+   <div class="row align-items-start">
+    <div class="col">
+      <div v-if="categorias.qtdeAtivos > categorias.maximoAmarelo" class="card text-bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Estoque adequado.</div>
+  <div class="card-body">
+    <h5 class="card-title">{{categorias.qtdeAtivos}}</h5>
+    <p class="card-text">{{ categorias.nomeCategoria }}</p>
+  </div>
+  </div>
+    </div>
+    <div class="col">
+     <div v-if="categorias.qtdeAtivos >= categorias.minimoAmarelo" class="card text-bg-warning mb-3" style="max-width: 18rem;">
+  <div class="card-header">Atenção</div>
+  <div class="card-body">
+    <h5 class="card-title">{{categorias.qtdeAtivos}}</h5>
+    <p class="card-text">{{ categorias.nomeCategoria }}</p>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div v-if="categorias.qtdeAtivos < categorias.minimoAmarelo" class="card text-bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Alerta de estoque</div>
+  <div class="card-body">
+    <h5 class="card-title">{{ categorias.qtdeAtivos }}</h5>
+    <p class="card-text">{{ categorias.nomeCategoria }}</p>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+   
           <hr>
           <!------------------------------------------------------------------------------------------------>
           <div class="container d-flex justify-content-between">
@@ -40,40 +43,12 @@
         <th scope="col">Ativos em atraso</th>
       </tr>
     </thead>
-    <!--<tbody v-for="ativo in AtivosLista" :key="ativo.id">
-      <tr>
-        <th scope="row"><span v-if="ativo.atrasado"></span>1</th>
-        <td>{{ativo.nome}}Ativo</td>
-        
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Ativo</td>
-        
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Ativo</td>
-        
-      </tr>
-    </tbody>
-    -->
+   
     <tbody>
-      <tr>
-        <th scope="row"><span></span>1</th>
-        <td>Ativo</td>
-        
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Ativo</td>
-        
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Ativo</td>
-        
-      </tr>
+      <tr v-for="item in ativosList" :key="item.id">
+        <th scope="row"><span></span>{{ item.id }}</th>
+        <td v-if="item.isAtrasado">{{ item.ativo.categoria.nomeCategoria }}</td>
+        </tr>
     </tbody>
   </table>
   <table class="table caption-top col-md-6 table-bordered ">
@@ -85,24 +60,11 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Ativo</td>
+      <tr v-for="item in ativosList" :key="item.id">
+        <th scope="row">{{ item.id }}</th>
+        <td v-if="item.dataDevolucao < today">{{item.ativo.categoria.nomeCategoria}}</td>
     
         <td><router-link type="button" to="/movimentacoes"><i class="bi bi-box-arrow-up-right"> </i></router-link></td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Ativo</td>
-        <td><router-link type="button" to="/movimentacoes"><i class="bi bi-box-arrow-up-right"> </i></router-link></td>
-        
-        
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Ativo</td>
-        <td><router-link type="button" to="/movimentacoes"><i class="bi bi-box-arrow-up-right"> </i></router-link></td>
-        
       </tr>
     </tbody>
   </table>
@@ -115,6 +77,8 @@
 
   <style scoped>
  @import url('../../../node_modules/bootstrap-icons/font/bootstrap-icons.css');
+ 
+ 
  
  /* As cores ficam muito fortes ativando isso. Sério.
  @import url('http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
@@ -130,22 +94,37 @@ thead th {
 import { defineComponent } from 'vue';
 import { MovimentacoesClient } from '@/client/movimentacao.client';
 import {Movimentacao} from '@/model/movimentacao'
+import { Categoria } from '@/model/categoria';
+import { CategoriaClient } from '@/client/categoria.client';
 
 export default defineComponent({
   name: 'AtivosLista',
   data(){
     return {
       ativosList: new Array <Movimentacao>(),
+      categoriasList: new Array <Categoria>(),
       movimentacao: new Movimentacao(),
-      
-
-
-
+      categorias: new Categoria(),
+      movimentacoesClient : new MovimentacoesClient(),
+      categoriasClient : new CategoriaClient(),
+      today : new Date(),
     }
+  },
+  mounted() {
+    this.findAll();
+  },
+  methods: {
+    findAll() {
+      this.movimentacoesClient.findAll()
+      .then (sucess =>{
+        this.ativosList = sucess;
+      })
+      .catch(error =>{
+        console.log(error);
+      });
+    },
+
   }
-
-
-
 })
 
 
