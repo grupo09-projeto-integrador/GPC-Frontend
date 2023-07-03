f89b4ee100a18921330e24022393d678f55f8b9d
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-03 14:29:53 -0300

    gerar pdf lista de espera

--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
@@ -3,6 +3,10 @@
         <div class="page-header d-flex justify-content-between align-items-center">
             <LinkDinamicoComponent routeList="/relatorios/listadeespera"
                 routeRegister="/relatorios/listadeespera/cadastrar" />
+            <button class="download btn btn-danger d-flex align-items-center mt-3 mr-3" id="downloadButton"
+                @click="downloadPDF" style="background-color: #DC3545;color: #fff;">
+                <i class="bi bi-download"></i>
+            </button>
         </div>
         <form class="form-app d-flex flex-column align-items-start gap-3 mt-4">
             <div class="d-flex align-items-center align-self-start gap-4">
@@ -11,43 +15,48 @@
                     <label for="dt_entrada">Categoria</label>
                     <select class="form-select" style="width: 300px" v-model="selectedCategory">
                         <option value="" selected>Todas as Categorias</option>
-  <option v-for="categoria in categoriaList" :value="categoria.nomeCategoria">{{ categoria.nomeCategoria }}</option>
-</select>
+                        <option v-for="categoria in categoriaList" :value="categoria.nomeCategoria">{{
+                            categoria.nomeCategoria }}</option>
+                    </select>
 
                 </div>
             </div>
         </form>
-        <table class="table table-sm table-bordered w-100 mt-4">
-      <thead>
-        <tr>
-          <th scope="col">Nome do Beneficiario</th>
-          <th scope="col">CPF do Beneficiario</th>
-          <th scope="col">Contato</th>
-          <th scope="col">Categoria</th>
-          <th scope="col">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="categoria in filter" :key="categoria.id">
-          <template v-for="pessoa in categoria.listaEspera">
-            <td>{{ pessoa.nome }}</td>
-            <td>{{ pessoa.cpf }}</td>
-            <td>{{ pessoa.telefone }}</td>
-            <td>{{ categoria.nomeCategoria }}</td>
-            <td>
-              <div class="d-flex justify-content-center actions">
-                <button class="btn btn-sm btn-primary me-2">
-                  <i class="bi bi-pencil-square" @click="editar()"></i> Editar
-                </button>
-                <button class="btn btn-sm btn-danger" @click="excluir()">
-                  <i class="bi bi-trash"></i> Excluir
-                </button>
-              </div>
-            </td>
-          </template>
-        </tr>
-      </tbody>
-    </table>
+        <table class=" printable table table-sm table-bordered w-100 mt-4 ml-2">
+            <thead>
+                <tr>
+                    <th scope="col">Nome do Beneficiario</th>
+                    <th scope="col">CPF do Beneficiario</th>
+                    <th scope="col">Contato</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col" class="hide">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="categoria in filter" :key="categoria.id">
+                    <template v-for="pessoa in categoria.listaEspera">
+                        <td>{{ pessoa.nome }}</td>
+                        <td>{{ pessoa.cpf }}</td>
+                        <td>{{ pessoa.telefone }}</td>
+                        <td>{{ categoria.nomeCategoria }}</td>
+                        <td>
+                        <td class="actions-cell">
+                            <div class="d-flex justify-content-center actions">
+                                <button class="btn btn-sm btn-primary me-2">
+                                    <i class="bi bi-pencil-square" @click="editar()"></i> Editar
+                                </button>
+                                <button class="btn btn-sm btn-danger" @click="excluir()"
+                                    style="background-color: #dc3545;color: #fff;">
+                                    <i class="bi bi-trash"></i> Excluir
+                                </button>
+                            </div>
+                        </td>
+
+                        </td>
+                    </template>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -57,54 +66,97 @@ import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
 import { Categoria } from '@/model/categoria';
 import { Pessoa } from '@/model/pessoa';
 import { CategoriaClient } from '@/client/categoria.client';
+import jsPDF from 'jspdf';
 
 export default defineComponent({
-  name: "ListaDeEsperaListar",
-  components: {
-    LinkDinamicoComponent,
-  },
-  data() {
-    return {
-      searchQuery: "",
-      categoriaList: [] as Categoria[],
-      tamanhoLista: 0 as number,
-      categoriaModel: new Categoria(),
-      selectedCategory: '',
-      pessoa: [] as Pessoa[],
-    };
-  },
-  mounted() {
-    // Fetch the categoriaList data
-    this.fetchCategoriaList();
-  },
-  computed: {
-  filter() {
-    if (this.selectedCategory === '') {
-      return this.categoriaList;
-    } else {
-      return this.categoriaList.filter((categoria) => {
-        return categoria.nomeCategoria === this.selectedCategory;
-      });
-    }
-  }
-},
+    name: "ListaDeEsperaListar",
+    components: {
+        LinkDinamicoComponent,
+    },
+    data() {
+        return {
+            searchQuery: "",
+            categoriaList: [] as Categoria[],
+            tamanhoLista: 0 as number,
+            categoriaModel: new Categoria(),
+            selectedCategory: '',
+            pessoa: [] as Pessoa[],
+        };
+    },
+    mounted() {
+        // Fetch the categoriaList data
+        this.fetchCategoriaList();
+    },
+    computed: {
+        filter() {
+            if (this.selectedCategory === '') {
+                return this.categoriaList;
+            } else {
+                return this.categoriaList.filter((categoria) => {
+                    return categoria.nomeCategoria === this.selectedCategory;
+                });
+            }
+        }
+    },
 
-  methods: {
-    fetchCategoriaList() {
-        const cliente = new CategoriaClient();
-      cliente.findAll().then((response) => {
-        this.categoriaList = response;
-      }).catch((error) => {
-        console.error(error);
-      });
+    methods: {
+        downloadPDF() {
+            const pdf = new jsPDF();
+            const element = document.querySelector('.printable') as HTMLElement;
+
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+            const widthRatio = pdfWidth / element.offsetWidth;
+            const heightRatio = pdfHeight / element.offsetHeight;
+            const scale = Math.min(widthRatio, heightRatio);
+
+            // Hide the actions cells
+    const actionsCells = element.querySelectorAll('.actions-cell') as NodeListOf<HTMLElement>;
+    actionsCells.forEach((cell) => {
+        cell.style.display = 'none';
+    });
+    const hide = element.querySelectorAll('.hide') as NodeListOf<HTMLElement>;
+        hide.forEach((cell) => {    
+            cell.style.display = 'none';
+        });
+
+
+
+            pdf.html(element, {
+                callback: function (pdf) {
+                    pdf.save(`ListaDeEspera.pdf`);
+
+                    actionsCells.forEach((cell) => {
+        cell.style.display = '';
+    });
+    const hide = element.querySelectorAll('.hide') as NodeListOf<HTMLElement>;
+        hide.forEach((cell) => {    
+            cell.style.display = '';
+        });
+                },
+                x: 0,
+                y: 0,
+                html2canvas: {
+                    scale: scale,
+                },
+            });
+        },
+
+        fetchCategoriaList() {
+            const cliente = new CategoriaClient();
+            cliente.findAll().then((response) => {
+                this.categoriaList = response;
+            }).catch((error) => {
+                console.error(error);
+            });
+        },
+        editar() {
+            // Edit logic
+        },
+        excluir() {
+            // Delete logic
+        },
     },
-    editar() {
-      // Edit logic
-    },
-    excluir() {
-      // Delete logic
-    },
-  },
 });
 
 </script>
@@ -167,4 +219,5 @@ td {
     border: 1px solid #dddddd;
     text-align: center;
     padding: 8px;
-}</style>
+}
+</style>
