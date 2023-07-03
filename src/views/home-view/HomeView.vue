f3f9a390d808ae53486a29d66ab19db432db9e80
vainicius <comercial.viniciusgabriel@gmail.com>
2023-07-03 01:24:52 -0300

    Cards criados

--- src/views/home-view/HomeView.vue
+++ src/views/home-view/HomeView.vue
@@ -4,15 +4,34 @@
       <div class="container text-center">
   <div class="row align-items-start">
     <div class="col">
-      One of three columns
+      <div class="card text-bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Success card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  </div>
     </div>
     <div class="col">
-      One of three columns
+     <div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Warning card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </div>
     <div class="col">
-      One of three columns
+      <div class="card text-bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Danger card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
     </div>
   </div>
+  
 </div>
    
           <hr>
