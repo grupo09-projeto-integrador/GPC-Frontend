ebb8e4a61fa3ba10a9379cb8c1003baa89b3190d
vainicius <comercial.viniciusgabriel@gmail.com>
2023-07-03 03:05:07 -0300

    Últimos empréstimos - DataDevolucao OK

--- src/client/movimentacao.client.ts
+++ src/client/movimentacao.client.ts
@@ -2,50 +2,69 @@ import { Movimentacao } from '@/model/movimentacao'
 import axios, { AxiosInstance } from 'axios'
 
 export class MovimentacoesClient {
-  private axiosClient: AxiosInstance
+  private axiosClient: AxiosInstance;
 
   constructor() {
     this.axiosClient = axios.create({
-      baseURL: 'http://localhost:8080/movimentacoes',
-      headers: { 'Content-type': 'application/json' }
-    })
+      baseURL: "http://localhost:8080/movimentacoes",
+      headers: { "Content-type": "application/json" },
+    });
   }
 
   public async findById(id: number): Promise<Movimentacao> {
     try {
-      return (await this.axiosClient.get<Movimentacao>(`/?id=${id}`)).data
+      return (await this.axiosClient.get<Movimentacao>(`/?id=${id}`)).data;
     } catch (error: any) {
-      return Promise.reject(error.response)
+      return Promise.reject(error.response);
     }
   }
 
-  public async findByDatePdf(dataEntrada: string, dataDevolucao: string): Promise<Movimentacao[]> {
+  public async findByDatePdf(
+    dataEntrada: string,
+    dataDevolucao: string
+  ): Promise<Movimentacao[]> {
     try {
-      return (await this.axiosClient.get<Movimentacao[]>(`pdf/${dataEntrada}/${dataDevolucao}`)).data
+      return (
+        await this.axiosClient.get<Movimentacao[]>(
+          `pdf/${dataEntrada}/${dataDevolucao}`
+        )
+      ).data;
     } catch (error: any) {
-      return Promise.reject(error.response)
+      return Promise.reject(error.response);
     }
   }
 
-  public async novaMovimentacao(movimentacao: Movimentacao): Promise<Movimentacao> {
+  public async novaMovimentacao(
+    movimentacao: Movimentacao
+  ): Promise<Movimentacao> {
     try {
-      return (await this.axiosClient.post<Movimentacao>('', movimentacao)).data
+      return (await this.axiosClient.post<Movimentacao>("", movimentacao)).data;
     } catch (error: any) {
-      return Promise.reject(error.response)
+      return Promise.reject(error.response);
     }
   }
   public async deletar(id: number): Promise<Movimentacao> {
     try {
-      return (await this.axiosClient.delete<Movimentacao>(`?id=${id}`,)).data
+      return (await this.axiosClient.delete<Movimentacao>(`?id=${id}`)).data;
     } catch (error: any) {
-      return Promise.reject(error.response)
+      return Promise.reject(error.response);
     }
   }
   public async filtrar(params: {}): Promise<Movimentacao[]> {
     try {
-      return (await this.axiosClient.get<Movimentacao[]>('/filtrar', { params })).data
+      return (
+        await this.axiosClient.get<Movimentacao[]>("/filtrar", { params })
+      ).data;
     } catch (error: any) {
-      return []
+      return [];
+    }
+  }
+  public async findAll(): Promise<Movimentacao[]> {
+    try {
+      return (await this.axiosClient.get<Movimentacao[]>(`movimentacao/lista`))
+        .data;
+    } catch (error: any) {
+      return Promise.reject(error.response);
     }
   }
 }
