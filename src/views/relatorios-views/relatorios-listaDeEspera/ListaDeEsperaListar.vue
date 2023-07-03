bc4d6c0cf0a9b28e907a4c7adf4c0f2a83230721
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-03 14:13:32 -0300

    Listar funcionando.

--- src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
+++ src/views/relatorios-views/relatorios-listaDeEspera/ListaDeEsperaListar.vue
@@ -3,53 +3,51 @@
         <div class="page-header d-flex justify-content-between align-items-center">
             <LinkDinamicoComponent routeList="/relatorios/listadeespera"
                 routeRegister="/relatorios/listadeespera/cadastrar" />
-            <div class="search-container mt-3">
-                <input type="text" class="search-input" placeholder="Buscar Por Nome do Beneficiario..."
-                    v-model="searchQuery" />
-                <i class="bi bi-search search-icon"></i>
-            </div>
         </div>
         <form class="form-app d-flex flex-column align-items-start gap-3 mt-4">
             <div class="d-flex align-items-center align-self-start gap-4">
 
                 <div class=" d-flex flex-column align-self-start">
                     <label for="dt_entrada">Categoria</label>
-                    <select class="form-select" style="width: 300px" v-model="categoriaModel">
-                    <option v-for="categoria in categoriaList" :value="categoria" >{{ categoria.nomeCategoria }}</option>
-                    </select>
+                    <select class="form-select" style="width: 300px" v-model="selectedCategory">
+                        <option value="" selected>Todas as Categorias</option>
+  <option v-for="categoria in categoriaList" :value="categoria.nomeCategoria">{{ categoria.nomeCategoria }}</option>
+</select>
+
                 </div>
             </div>
         </form>
         <table class="table table-sm table-bordered w-100 mt-4">
-            <thead>
-                <tr>
-                    <th scope="col">Nome do Beneficiario</th>
-                    <th scope="col">CPF do Beneficiario</th>
-                    <th scope="col">Contato</th>
-                    <th scope="col">Categoria</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                <tr v-for="list in categoriaFiltrada.listaEspera" :key="tamanhoLista">
-            <td> {{ list.nome }} </td>
-            <td>{{ list.cpf }}</td>
-            <td>{{ list.telefone }}</td>
-            <td>{{ categoriaFiltrada.nomeCategoria }}</td>
+      <thead>
+        <tr>
+          <th scope="col">Nome do Beneficiario</th>
+          <th scope="col">CPF do Beneficiario</th>
+          <th scope="col">Contato</th>
+          <th scope="col">Categoria</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="categoria in filter" :key="categoria.id">
+          <template v-for="pessoa in categoria.listaEspera">
+            <td>{{ pessoa.nome }}</td>
+            <td>{{ pessoa.cpf }}</td>
+            <td>{{ pessoa.telefone }}</td>
+            <td>{{ categoria.nomeCategoria }}</td>
             <td>
               <div class="d-flex justify-content-center actions">
                 <button class="btn btn-sm btn-primary me-2">
-                  <i class="bi bi-pencil-square"></i> Editar </button>
-                <button class="btn btn-sm btn-danger" @click="excluir(list.id)">
-                  <i class="bi bi-trash"></i> Excluir </button>
+                  <i class="bi bi-pencil-square" @click="editar()"></i> Editar
+                </button>
+                <button class="btn btn-sm btn-danger" @click="excluir()">
+                  <i class="bi bi-trash"></i> Excluir
+                </button>
               </div>
             </td>
-
-          </tr>
-
-            </tbody>
-        </table>
+          </template>
+        </tr>
+      </tbody>
+    </table>
     </div>
 </template>
 
@@ -59,93 +57,58 @@ import LinkDinamicoComponent from "@/components/LinkDinamicoComponent.vue";
 import { Categoria } from '@/model/categoria';
 import { Pessoa } from '@/model/pessoa';
 import { CategoriaClient } from '@/client/categoria.client';
+
 export default defineComponent({
-    name: "ListaDeEsperaListar",
-    components: {
-        LinkDinamicoComponent,
-    },
-    data() {
-        return {
-            searchQuery: "",
-            categoriaList: [] as Categoria[],
-            categoriaFiltrada: new Categoria,
-            tamanhoLista: 0 as number,
-
-            categoriaModel: new Categoria,
-            pessoa: [] as Pessoa[],
-            nivelUrgencia: [
-                { id: 1, nome: 'Alta' },
-                { id: 2, nome: 'Média' },
-                { id: 3, nome: 'Baixa' },
-            ],
-        };
-    },
-
-    mounted(){
-
-        this.findCategorias()
-
-    },
-
-    methods: {
-
-        async findCategorias(){
-
-        const categoriaClient = new CategoriaClient
-
-        categoriaClient.findAll()
-        .then(sucess => {
-
-        this.categoriaList = sucess
-
-        }
-        )
-        .catch(error => {
-        console.log(error);
-        }); 
-        },
-
-        async findLista(){
-
-        const categoriaClient = new CategoriaClient
-
-        categoriaClient.findById(this.categoriaModel.id).then(sucess => {
-
-            this.categoriaFiltrada = sucess
-
-        }).catch(error => {
-        console.log(error);
-        }); 
-
-        this.tamanhoLista = this.categoriaFiltrada.listaEspera.length
-
-        },
-
-
-
-async excluir(id: number){
-  const confirmation = confirm("Você tem certeza de que deseja remover essa pessoa da lista de espera?");
-    if (!confirmation) {
-      return;
+  name: "ListaDeEsperaListar",
+  components: {
+    LinkDinamicoComponent,
+  },
+  data() {
+    return {
+      searchQuery: "",
+      categoriaList: [] as Categoria[],
+      tamanhoLista: 0 as number,
+      categoriaModel: new Categoria(),
+      selectedCategory: '',
+      pessoa: [] as Pessoa[],
+    };
+  },
+  mounted() {
+    // Fetch the categoriaList data
+    this.fetchCategoriaList();
+  },
+  computed: {
+  filter() {
+    if (this.selectedCategory === '') {
+      return this.categoriaList;
+    } else {
+      return this.categoriaList.filter((categoria) => {
+        return categoria.nomeCategoria === this.selectedCategory;
+      });
     }
+  }
+},
 
-    try {
-
-      const categoriaClient = new CategoriaClient();
-      await categoriaClient.delete(id);
-      window.location.reload();
-    } catch (error) {
-      console.error(error);
-    }
-
-
-
-}
-
-    }
-
+  methods: {
+    fetchCategoriaList() {
+        const cliente = new CategoriaClient();
+      cliente.findAll().then((response) => {
+        this.categoriaList = response;
+      }).catch((error) => {
+        console.error(error);
+      });
+    },
+    editar() {
+      // Edit logic
+    },
+    excluir() {
+      // Delete logic
+    },
+  },
 });
+
 </script>
+
 
 <style>
 .page-header {
