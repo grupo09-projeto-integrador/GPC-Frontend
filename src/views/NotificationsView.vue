5f2c570607fe3a7847ba5e168898ccccc5404f88
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-02 22:51:31 -0300

    data devolucao notification done.

--- src/views/NotificationsView.vue
+++ src/views/NotificationsView.vue
@@ -1,148 +1,198 @@
 <template>
-    <div class="main-content">
-      <div class="header p-3 d-flex justify-content-between align-items-center">
-        <div class="rightHeader">
-          <div class="title">Notificações</div>
-          <div class="subtitle">Você tem {{ nbNotifications }} notificações para resolver.</div>
-        </div>
-        <div class="leftHeader">
-          <button class="btn btn-primary" style="border-radius: 30px; padding: 6px 14px;" @click="resetNotifications">Marcar como lido</button>
-        </div>
+  <div class="main-content">
+    <div class="header p-3 d-flex justify-content-between align-items-center">
+      <div class="rightHeader">
+        <div class="title">Notificações</div>
+        <div class="subtitle">Você tem {{ nbNotifications }} notificações para resolver.</div>
       </div>
-      <div class="body p-3">
-        <template v-for="(notification, index) in notifications" :key="notification.id">
-          <div v-if="shouldDisplayDate(notification, index)" class="notification-date">{{ getFormattedDate(notification.date) }}</div>
-          <div :class="['notification', { 'notification-read': notification.read }]" class="d-flex flex-column">
-            <div class="notification-title">{{ notification.title }}</div>
-            <div class="notification-body">{{ notification.body }}</div>
-          </div>
-        </template>
+      <div class="leftHeader">
+        <button class="btn btn-primary" style="border-radius: 30px; padding: 6px 14px;" @click="resetNotifications">Marcar
+          como lido</button>
       </div>
     </div>
-  </template>
+    <div class="body p-3">
+      <template v-for="(notification, index) in notifications" :key="notification.id">
+        <div v-if="shouldDisplayDate(notification, index)" class="notification-date">{{
+          getFormattedDate(notification.date) }}</div>
+        <div :class="['notification', { 'notification-read': notification.read }]" class="d-flex flex-column">
+          <div class="notification-title">{{ notification.title }}</div>
+          <div class="notification-body">{{ notification.body }}</div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
   
-  <script lang="ts">
-  export default {
-    data() {
-      return {
-        notifications: [
-          {
-            id: 1,
-            title: "Notification 1",
-            body: "This is the body of notification 1.",
-            date: new Date("2023-06-30"), // Example date
-            read: false, // New notification is not read
-          },
-          {
-            id: 2,
-            title: "Notification 2",
-            body: "This is the body of notification 2.",
-            date: new Date("2023-06-30"), // Example date
-            read: true, // Already read notification
-          },
-          {
-            id: 3,
-            title: "Notification 3",
-            body: "This is the body of notification 3.",
-            date: new Date("2023-07-01"), // Example date
-            read: false, // New notification is not read
-          },
-        ],
-        nbNotifications: 0,
-      };
+<script lang="ts">
+import { Ativo } from '@/model/ativo';
+import { Beneficiario } from '@/model/beneficiario';
+import { Categoria } from '@/model/categoria';
+import { Movimentacao } from '@/model/movimentacao';
+import { defineComponent } from 'vue';
+import { AtivoClient } from '@/client/ativo.client';
+import { MovimentacoesClient } from '@/client/movimentacao.client';
+import { CategoriaClient } from '@/client/categoria.client';
+
+interface Notification {
+  title: string;
+  body: string;
+  date: Date;
+  read: boolean;
+}
+
+export default defineComponent({
+  data() {
+    return {
+      notifications: [] as Notification[],
+      movimentacoes: [] as Movimentacao[],
+      beneficiarios: [] as Beneficiario[],
+      ativos: [] as Ativo[],
+      categories: [] as Categoria[],
+      nbNotifications: 0,
+    };
+  },
+  mounted() {
+    this.updateNotificationCount();
+    this.dataDeliveryAlert();
+    this.qtdeAtivos();
+  },
+  methods: {
+    shouldDisplayDate(notification: Notification, index: number) {
+      if (index === 0) {
+        return true;
+      }
+      const currentNotification = this.notifications[index];
+      const previousNotification = this.notifications[index - 1];
+
+      return !this.isSameDate(currentNotification.date, previousNotification.date);
     },
-    mounted() {
-      this.updateNotificationCount(); // Update the count when the component is mounted
+    isSameDate(date1: Date, date2: Date) {
+      return (
+        date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate()
+      );
     },
-    methods: {
-      shouldDisplayDate(notification: any, index: number) {
-        if (index === 0) {
-          return true;
+    getFormattedDate(date: Date) {
+      return date.toDateString();
+    },
+    async updateNotificationCount() {
+      this.nbNotifications = this.notifications.filter((notification) => !notification.read).length;
+    },
+    async resetNotifications() {
+      this.notifications.forEach((notification) => {
+        notification.read = true;
+      });
+      this.updateNotificationCount();
+    },
+    dataDeliveryAlert() {
+      const move = new MovimentacoesClient();
+      const today = new Date(); // Get today's date
+      const oneDayBeforeToday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1); // Calculate one day before today
+      console.log(oneDayBeforeToday);
+
+      move.findAll().then((res) => {
+        this.movimentacoes = res;
+
+        console.log(this.movimentacoes);
+
+        // Check each movimentaco for the condition
+        for (const movimentaco of this.movimentacoes) {
+          const datadevoluco = new Date(movimentaco.dataDevolucao);
+          datadevoluco.setDate(datadevoluco.getDate() + 1);
+          datadevoluco.setHours(0, 0, 0, 0)
+
+
+          console.log(datadevoluco)
+
+          if (datadevoluco.getTime() === oneDayBeforeToday.getTime()) {
+            const notification = {
+              title: 'Aviso de Devolução Próxima',
+              body: "O ativo com o ID de patrimônio " + movimentaco.ativo.idPatrimonio + " atribuído ao beneficiário " + movimentaco.beneficiario.perfil.nome + " tem a data de devolução prevista para amanhã.",
+              date: new Date(),
+              read: false
+            };
+
+            this.notifications.push(notification);
+          }
         }
-  
-        const currentNotification = this.notifications[index];
-        const previousNotification = this.notifications[index - 1];
-  
-        return !this.isSameDate(currentNotification.date, previousNotification.date);
-      },
-      isSameDate(date1: any, date2: any) {
-        return (
-          date1.getFullYear() === date2.getFullYear() &&
-          date1.getMonth() === date2.getMonth() &&
-          date1.getDate() === date2.getDate()
-        );
-      },
-      getFormattedDate(date: any) {
-        return date.toDateString();
-      },
-      updateNotificationCount() {
-        this.nbNotifications = this.notifications.filter((notification) => !notification.read).length; 
-      },
-      resetNotifications() {
-        this.notifications.forEach((notification) => {
-          notification.read = true; 
-        });
-        this.updateNotificationCount(); 
-      },
+
+        this.updateNotificationCount();
+      });
     },
-  };
-  </script>
-  <style scoped>
-  .header {
-      border-bottom: 1px solid #e6e6e6;
-  }
-  
-  .title {
-      font-size: 24px;
-      font-weight: 600;
-      color: #333;
-  }
-  
-  .subtitle {
-      font-size: 16px;
-      color: #777;
-  }
-  
-  .body {
-    background-color: #f9f9f9;
-    padding: 20px;
-    border-radius: 8px;
-  }
+    qtdeAtivos() {
+      const categoria = new CategoriaClient();
+      categoria.findAll().then((res) => {
+        this.categories = res;
 
-  .notification-date {
-    font-size: 14px;
-    color: #999;
-    margin-bottom: 10px;
-  }
+        console.log(this.categories);
 
-  .notification {
-    margin-bottom: 20px;
-    background-color: #fff;
-    padding: 15px;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    transition: box-shadow 0.3s ease;
-  }
+      
+          
+        
+      });
+    }
 
-  .notification:hover {
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
+  },
+});
+</script>
 
-  .notification-title {
-    font-size: 18px;
-    font-weight: 600;
-    color: #333;
-    margin-bottom: 5px;
-  }
+<style scoped>
+.header {
+  border-bottom: 1px solid #e6e6e6;
+}
 
-  .notification-body {
-    font-size: 14px;
-    color: #666;
-    line-height: 1.5;
-  }
+.title {
+  font-size: 24px;
+  font-weight: 600;
+  color: #333;
+}
 
-  .notification-read {
+.subtitle {
+  font-size: 16px;
+  color: #777;
+}
+
+.body {
+  background-color: #f9f9f9;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.notification-date {
+  font-size: 14px;
+  color: #999;
+  margin-bottom: 10px;
+}
+
+.notification {
+  margin-bottom: 20px;
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease;
+}
+
+.notification:hover {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.notification-title {
+  font-size: 18px;
+  font-weight: 600;
+  color: #333;
+  margin-bottom: 5px;
+}
+
+.notification-body {
+  font-size: 14px;
+  color: #666;
+  line-height: 1.5;
+}
+
+.notification-read {
   background-color: #f1f1f1;
 }
-  </style>
+</style>
   
